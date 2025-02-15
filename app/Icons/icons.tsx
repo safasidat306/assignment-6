a17c6bc9f28c9ca54b13edf9bfa65b0d0c0a4cc4
Safa Nasir Sidat <nasirsidat650@gmail.com>
2025-02-15 22:12:09 +0500

    code uploaded

--- app/Icons/icons.tsx
+++ app/Icons/icons.tsx
@@ -146,7 +146,7 @@ export const Logo6 : React.FC<IconProps> = () => (<svg width="33" height="32" vi
   
 );  
 
-export const Logo7 : React.FC<IconProps> = ({ className, ...rest }) => (<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+export const Logo7 : React.FC<IconProps> = () => (<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M10.6666 29.3333H21.3333C26.6933 29.3333 27.6533 27.1867 27.9333 24.5733L28.9333 13.9067C29.2933 10.6533 28.36 8 22.6666 8H9.33331C3.63997 8 2.70664 10.6533 3.06664 13.9067L4.06664 24.5733C4.34664 27.1867 5.30664 29.3333 10.6666 29.3333Z" stroke="#292D32" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
   <path d="M10.6667 7.99996V6.93329C10.6667 4.57329 10.6667 2.66663 14.9334 2.66663H17.0667C21.3334 2.66663 21.3334 4.57329 21.3334 6.93329V7.99996" stroke="#292D32" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
   <path d="M18.6666 17.3333V18.6667C18.6666 18.68 18.6666 18.68 18.6666 18.6933C18.6666 20.1467 18.6533 21.3333 16 21.3333C13.36 21.3333 13.3333 20.16 13.3333 18.7067V17.3333C13.3333 16 13.3333 16 14.6666 16H17.3333C18.6666 16 18.6666 16 18.6666 17.3333Z" stroke="#292D32" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
@@ -308,7 +308,7 @@ export const TwitterIcon2: React.FC<IconProps> = ({...rest }) => (
 </svg>
 );
 
-export const Rating: React.FC<IconProps> = ({className, ...rest }) => (
+export const Rating: React.FC<IconProps> = ({...rest }) => (
   <svg
   xmlns="http://www.w3.org/2000/svg"
   xmlSpace="preserve"
